e09f21c4bd83f21156b1cfa35b8952aed2ecfa92
BoyWithSilverWings <agney@outlook.in>
2018-12-26 12:27:06 +0530

    Add a custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Smiley = styled.p`
+  font-size: 10rem;
+`;
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <Smiley>🤦🏻‍♂️</Smiley>
+      <h1>NOT FOUND</h1>
+      <p>I have not added this page yet.</p>
+      <p>
+        <span>Ask me to add it &nbsp;</span>
+        <span>
+          <a
+            href="https://twitter.com/agneymenon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @agneymenon
+          </a>
+        </span>
+      </p>
+    </Container>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
