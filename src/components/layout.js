d02d046a9cdde88c1a9fc7628b20a745a4e8b6c6
Agney <agney@outlook.in>
2020-01-05 08:26:29 +0530

    move global styles to new file

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,70 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { createGlobalStyle } from 'styled-components';
-import { StaticQuery, graphql, useStaticQuery } from 'gatsby';
+import styled from 'styled-components';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Header from './header';
 import media from '../utils/media';
-
-const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: system;
-    font-style: normal;
-    font-weight: 300;
-    src: local('.SFNSText-Light'), local('.HelveticaNeueDeskInterface-Light'),
-      local('.LucidaGrandeUI'), local('Ubuntu Light'), local('Segoe UI Light'),
-      local('Roboto-Light'), local('DroidSans'), local('Tahoma');
-  }
-
-  :root {
-    font-size: 10px;
-  }
-
-  body {
-    font-family: 'system';
-    margin: 0;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    color: rgba(0, 0, 0, 0.8);
-    min-height: 100vh;
-    position: relative;
-    font-size: 1.6rem;
-  }
-
-  ::selection {
-    background-color: #08B2E3;
-    color: #FFFFFF;
-  }
-
-  h1, h2, h3, h4, h5, h6 {
-    font-family: 'Aleo', serif;
-  }
-
-  h1::selection, h2::selection, h3::selection, h4::selection, h5::selection, h6::selection {
-    background-color: #DA344D;
-  }
-  
-  h2 {
-    font-size: 2.5rem;
-  }
-
-  h3 {
-    font-size: 2.4rem;
-  }
-
-  h4 {
-    font-size: 1.6rem;
-  }
-  
-  code {
-    font-family: Menlo,Monaco,"Courier New",Courier,monospace;
-    word-break: break-word;
-  }
-
-  pre code {
-    word-break: normal;
-  }
-`;
+import GlobalStyles from '../utils/global';
 
 const Content = styled.div`
   width: 60%;
---
+++ src/utils/global.js
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: system;
+    font-style: normal;
+    font-weight: 300;
+    src: local('.SFNSText-Light'), local('.HelveticaNeueDeskInterface-Light'),
+      local('.LucidaGrandeUI'), local('Ubuntu Light'), local('Segoe UI Light'),
+      local('Roboto-Light'), local('DroidSans'), local('Tahoma');
+  }
+
+  :root {
+    font-size: 10px;
+  }
+
+  body {
+    font-family: 'system';
+    margin: 0;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    color: rgba(0, 0, 0, 0.8);
+    min-height: 100vh;
+    position: relative;
+    font-size: 1.6rem;
+  }
+
+  ::selection {
+    background-color: #08B2E3;
+    color: #FFFFFF;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Aleo', serif;
+  }
+
+  h1::selection, h2::selection, h3::selection, h4::selection, h5::selection, h6::selection {
+    background-color: #DA344D;
+  }
+  
+  h2 {
+    font-size: 2.5rem;
+  }
+
+  h3 {
+    font-size: 2.4rem;
+  }
+
+  h4 {
+    font-size: 1.6rem;
+  }
+  
+  code {
+    font-family: Menlo,Monaco,"Courier New",Courier,monospace;
+    word-break: break-word;
+  }
+
+  pre code {
+    word-break: normal;
+  }
+`;
+
+export default GlobalStyles;
