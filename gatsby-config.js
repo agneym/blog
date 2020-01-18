baf4171986316e93c50f6a254fecef87e550d82a
Agney <agney@outlook.in>
2020-01-18 22:30:44 +0530

    Add rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     description: `A techxploration - Personal blog by Agney`,
     author: `Agney Menon`,
     authorTagline: 'Engineer. Driven by Passion',
+    siteUrl: `https://blog.agney.dev`,
     social: {
       twitter: `agneymenon`,
     },
@@ -80,5 +81,58 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  data: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.body }],
+                });
+              });
+            },
+            query: `
+            {
+              allMdx(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+                filter: { frontmatter: { published: { ne: false } } }
+              ) {
+                edges {
+                  node {
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                    rawBody
+                  }
+                }
+              }
+            }
+            `,
+            output: '/rss.xml',
+            title: `Mindless - Agney's Technical Blog RSS Feed`,
+          },
+        ],
+      },
+    },
   ],
 };
