707b9e759eeb04714ebff5be788428d88641a620
Agney <agney.menon@bigbinary.com>
2020-02-14 22:55:33 +0530

    correct coffee import

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -12,7 +12,7 @@ import AnimatedLink from '../utils/animated-link';
 import 'react-js-live/build/main.css';
 import CodeViewer from '../components/code-viewer';
 import FixedMessage from '../components/fixed-message';
-import Coffee from '../components/Coffee';
+import Coffee from '../components/ccocoffee';
 import Comments from '../components/comments';
 
 const components = {
