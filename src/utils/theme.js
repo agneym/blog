7e5f635f25d3704ec2deb1b9a2acaf7eaf233835
Agney <agney.menon@bigbinary.com>
2020-02-14 23:20:28 +0530

    Add theme colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,6 +2,16 @@ const theme = {
   nav: {
     height: '6rem',
   },
+  colors: {
+    background: {
+      dark: `#121212`,
+      light: `#FFFFFF`,
+    },
+    textColor: {
+      dark: `rgba(255, 255, 255, 0.85)`,
+      light: `rgba(0, 0, 0, 0.8)`,
+    },
+  },
 };
 
 export default theme;
