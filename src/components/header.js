776d222c5a13000366bc6e1448bac1e32a3c3680
Agney <agney.menon@bigbinary.com>
2020-02-22 23:12:21 +0530

    add feather icons

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import DarkModeToggle from 'react-dark-mode-toggle';
+import { Mail, Rss } from 'react-feather';
 
 import StyledLink from '../utils/styled-link';
 import media from '../utils/media';
@@ -46,9 +47,15 @@ const IconContainer = styled.div`
   }
 `;
 
-const Icon = styled.svg`
+const IconLink = styled.a`
   height: 2rem;
   color: ${props => props.theme.colors.textColor};
+  text-decoration: none;
+
+  &:active,
+  &:visited {
+    color: inherit;
+  }
 `;
 
 const Header = ({ title, mode }) => {
@@ -58,25 +65,20 @@ const Header = ({ title, mode }) => {
         <Title>{title}</Title>
       </TitleLink>
       <IconContainer>
-        <a
+        <IconLink
           href="#newsletter-title"
           title="Subscribe to Newsletter"
           aria-label="Subscribe to Newsletter"
         >
-          <Icon
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="feather feather-mail"
-          >
-            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-            <polyline points="22,6 12,13 2,6"></polyline>
-          </Icon>
-        </a>
+          <Mail />
+        </IconLink>
+        <IconLink
+          href="https://blog.agney.dev/rss.xml"
+          title="Subscribe via RSS"
+          aria-label="Subscribe via RSS"
+        >
+          <Rss />
+        </IconLink>
         <DarkModeToggle onChange={mode.toggle} checked={mode.value} size={50} />
       </IconContainer>
     </Container>
